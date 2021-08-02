5d51661661553e44313083de667879bcc9a4feaa
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-08-02 15:56:04 +0700

    add: firebase analytics

--- firebase/src/main/java/com/frogobox/firebase/MainActivity.kt
+++ firebase/src/main/java/com/frogobox/firebase/MainActivity.kt
@@ -2,10 +2,18 @@ package com.frogobox.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private val firebaseAnalytics: FirebaseAnalytics by lazy {
+        FirebaseAnalytics.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
