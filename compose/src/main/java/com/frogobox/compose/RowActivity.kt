27c498fd6318ff8ea160d11c365f164260fb1ccd
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-07 19:27:22 +0700

    add: Row and Column

--- compose/src/main/java/com/frogobox/compose/RowActivity.kt
+++ compose/src/main/java/com/frogobox/compose/RowActivity.kt
@@ -3,29 +3,40 @@ package com.frogobox.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
-class ColumnActivity : ComponentActivity() {
+/*
+ * Created by faisalamir on 07/09/21
+ * CaravanAndroid
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+class RowActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColumnActivityUI()
+            RowActivityUI()
         }
     }
 }
-
 
 @Preview(
     showBackground = true,
     showSystemUi = true
 )
 @Composable
-fun ColumnActivityUI() {
-    Column() {
+fun RowActivityUI() {
+    Row() {
         Text(text = "Learning Jetpack Compose")
         Text(text = "Hello Jetpack Compose")
     }
