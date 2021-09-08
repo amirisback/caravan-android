1c076978da9d96e99544117d50def02df680bf43
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-08 11:53:02 +0700

    update: lazy column

---
+++ compose/src/main/java/com/frogobox/compose/ExampleData.kt
@@ -0,0 +1,18 @@
+package com.frogobox.compose
+
+/*
+ * Created by faisalamir on 08/09/21
+ * CaravanAndroid
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+data class ExampleData(
+    val name: String,
+    val role: String
+)
--- compose/src/main/java/com/frogobox/compose/LazyColumnActivity.kt
+++ compose/src/main/java/com/frogobox/compose/LazyColumnActivity.kt
@@ -3,10 +3,10 @@ package com.frogobox.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 /*
  * Created by faisalamir on 08/09/21
@@ -25,19 +25,37 @@ class LazyColumnActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Lazy()
+            Lazy(setupData())
         }
+    }
+
+    private fun setupData() : MutableList<ExampleData> {
+        val data = mutableListOf<ExampleData>()
+        data.add(ExampleData("Amir, Is Back", "Android Programmer"))
+        data.add(ExampleData("Amir, Is Back", "Android Programmer"))
+        data.add(ExampleData("Amir, Is Back", "Android Programmer"))
+        data.add(ExampleData("Amir, Is Back", "Android Programmer"))
+        return data
+    }
+
+
+}
+
+@Composable
+fun Lazy(data: List<ExampleData>, ) {
+    LazyColumn {
+
+        items(data.size) { index ->
+            ListMessage(data[index])
+        }
+
     }
 }
 
-@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun Lazy() {
-    LazyColumn {
-
-        // Add 5 items
-        items(5) { index ->
-            Text(text = "Item: $index")
-        }
+fun ListMessage(data: ExampleData) {
+    Column() {
+        Text(text = data.name)
+        Text(text = data.role)
     }
 }
