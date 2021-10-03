1aeaf28b8828955aea499f7159b2572a20b42961
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-10-04 00:51:29 +0700

    add: remember state

--- compose/src/main/java/com/frogobox/compose/LazyColumnActivity.kt
+++ compose/src/main/java/com/frogobox/compose/LazyColumnActivity.kt
@@ -1,12 +1,21 @@
 package com.frogobox.compose
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.frogobox.api.core.ConsumeApiResponse
+import com.frogobox.api.news.ConsumeNewsApi
+import com.frogobox.api.news.model.Article
+import com.frogobox.api.news.response.ArticleResponse
+import com.frogobox.api.news.util.NewsConstant.CATEGORY_HEALTH
+import com.frogobox.api.news.util.NewsConstant.COUNTRY_ID
+import com.frogobox.api.news.util.NewsUrl
 
 /*
  * Created by faisalamir on 08/09/21
@@ -25,11 +34,11 @@ class LazyColumnActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Lazy(setupData())
+            ActivityContent()
         }
     }
 
-    private fun setupData() : MutableList<ExampleData> {
+    private fun setupData(): MutableList<ExampleData> {
         val data = mutableListOf<ExampleData>()
         data.add(ExampleData("Amir, Is Back", "Android Programmer"))
         data.add(ExampleData("Amir, Is Back", "Android Programmer"))
@@ -42,9 +51,48 @@ class LazyColumnActivity : ComponentActivity() {
 }
 
 @Composable
-fun Lazy(data: List<ExampleData>, ) {
-    LazyColumn {
+fun ActivityContent() {
 
+    var newsState : List<Article> by remember {
+        mutableStateOf(emptyList())
+    }
+
+    val consumeNewsApi = ConsumeNewsApi(NewsUrl.API_KEY)
+    consumeNewsApi.usingChuckInterceptor(LocalContext.current) // Using Chuck Interceptor
+    consumeNewsApi.getTopHeadline( // Adding Base Parameter on main function
+        null,
+        null,
+        CATEGORY_HEALTH,
+        COUNTRY_ID,
+        null,
+        null,
+        object : ConsumeApiResponse<ArticleResponse> {
+            override fun onSuccess(data: ArticleResponse) {
+                // Your Ui or data
+                newsState = data.articles!!
+            }
+
+            override fun onFailed(statusCode: Int, errorMessage: String?) {
+                // Your failed to do
+            }
+
+            override fun onShowProgress() {
+                // Your Progress Show
+            }
+
+            override fun onHideProgress() {
+                // Your Progress Hide
+            }
+
+        })
+
+    Lazy(newsState)
+
+}
+
+@Composable
+fun Lazy(data: List<Article>) {
+    LazyColumn {
         items(data.size) { index ->
             ListMessage(data[index])
         }
@@ -53,9 +101,9 @@ fun Lazy(data: List<ExampleData>, ) {
 }
 
 @Composable
-fun ListMessage(data: ExampleData) {
+fun ListMessage(data: Article) {
     Column() {
-        Text(text = data.name)
-        Text(text = data.role)
+        data.title?.let { Text(text = "- $it") }
+
     }
 }
