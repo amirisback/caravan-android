a5f09df41ee8add937a909af5ba882a63b8f312d
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-11-02 10:57:01 +0700

    update: build.gradle

--- compose/src/main/java/com/frogobox/compose/LazyColumnActivity.kt
+++ compose/src/main/java/com/frogobox/compose/LazyColumnActivity.kt
@@ -1,13 +1,16 @@
 package com.frogobox.compose
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import com.frogobox.api.core.ConsumeApiResponse
 import com.frogobox.api.news.ConsumeNewsApi
@@ -47,13 +50,12 @@ class LazyColumnActivity : ComponentActivity() {
         return data
     }
 
-
 }
 
 @Composable
 fun ActivityContent() {
 
-    var newsState : List<Article> by remember {
+    var newsState: List<Article> by remember {
         mutableStateOf(emptyList())
     }
 
@@ -104,6 +106,5 @@ fun Lazy(data: List<Article>) {
 fun ListMessage(data: Article) {
     Column() {
         data.title?.let { Text(text = "- $it") }
-
     }
 }
